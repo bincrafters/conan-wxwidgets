47eadcf5bf337ea1eea4d38e05b309c8c7ce0040
SSE4 <tomskside@gmail.com>
2018-07-08 16:24:44 +0700

    - get rid of tools.collect_libs
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -20,27 +20,51 @@ class wxWidgetsConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False],
                "fPIC": [True, False],
+               "unicode": [True, False],
+               "compatibility": ["2.8", "3.0", "3.1"],
                "zlib": [None, "system", "zlib"],
                "png": [None, "system", "libpng"],
                "jpeg": [None, "system", "libjpeg", "libjpeg-turbo", "mozjpeg"],
                "tiff": [None, "system", "libtiff"],
                "expat": [None, "system", "expat"],
                "secretstore": [True, False],
+               "aui": [True, False],
                "opengl": [True, False],
-               "webview": [True, False],
+               "html": [True, False],
                "mediactrl": [True, False],  # disabled by default as wxWidgets still uses deprecated GStreamer 0.10
+               "propgrid": [True, False],
+               "debugreport": [True, False],
+               "ribbon": [True, False],
+               "richtext": [True, False],
+               "sockets": [True, False],
+               "stc": [True, False],
+               "webview": [True, False],
+               "xml": [True, False],
+               "xrc": [True, False],
                "cairo": [True, False]}
     default_options = "shared=False",\
                       "fPIC=True",\
+                      "unicode=True",\
+                      "compatibility=3.0",\
                       "zlib=zlib",\
                       "png=libpng",\
                       "jpeg=libjpeg",\
                       "tiff=libtiff",\
                       "expat=expat",\
                       "secretstore=True",\
+                      "aui=True",\
                       "opengl=True",\
-                      "webview=True",\
+                      "html=True",\
                       "mediactrl=False",\
+                      "propgrid=True",\
+                      "debugreport=True",\
+                      "ribbon=True",\
+                      "richtext=True",\
+                      "sockets=True",\
+                      "stc=True",\
+                      "webview=True",\
+                      "xml=True",\
+                      "xrc=True",\
                       "cairo=True"
     source_subfolder = "source_subfolder"
     build_subfolder = "build_subfolder"
@@ -59,8 +83,8 @@ class wxWidgetsConan(ConanFile):
                     arch_suffix = ':i386'
                 elif self.settings.arch == 'x86_64':
                     arch_suffix = ':amd64'
-                packages = ['libx11-dev%s' % arch_suffix]
-                packages.append('libgtk2.0-dev%s' % arch_suffix)
+                packages = ['libx11-dev%s' % arch_suffix,
+                            'libgtk2.0-dev%s' % arch_suffix]
                 # TODO : GTK3
                 # packages.append('libgtk-3-dev%s' % arch_suffix)
                 if self.options.secretstore:
@@ -115,32 +139,52 @@ class wxWidgetsConan(ConanFile):
         def option_value(option):
             return 'OFF' if option is None else 'sys'
         cmake = CMake(self)
+
+        # generic build options
         cmake.definitions['wxBUILD_SHARED'] = self.options.shared
         cmake.definitions['wxBUILD_SAMPLES'] = 'OFF'
         cmake.definitions['wxBUILD_TESTS'] = 'OFF'
         cmake.definitions['wxBUILD_DEMOS'] = 'OFF'
         cmake.definitions['wxBUILD_INSTALL'] = True
+        cmake.definitions['wxBUILD_COMPATIBILITY'] = self.options.compatibility
 
+        # platform-specific options
         if self.settings.compiler == 'Visual Studio':
             cmake.definitions['wxBUILD_USE_STATIC_RUNTIME'] = 'MT' in str(self.settings.compiler.runtime)
             cmake.definitions['wxBUILD_MSVC_MULTIPROC'] = True
-
-        cmake.definitions['wxUSE_LIBPNG'] = option_value(self.options.png)
-        cmake.definitions['wxUSE_LIBJPEG'] = option_value(self.options.jpeg)
-        cmake.definitions['wxUSE_LIBTIFF'] = option_value(self.options.tiff)
-        cmake.definitions['wxUSE_ZLIB'] = option_value(self.options.zlib)
-        cmake.definitions['wxUSE_EXPAT'] = option_value(self.options.expat)
-
-        cmake.definitions['wxUSE_SECRETSTORE'] = self.options.secretstore
-        cmake.definitions['wxUSE_OPENGL'] = self.options.opengl
-        cmake.definitions['wxUSE_WEBVIEW'] = self.options.webview
-        cmake.definitions['wxUSE_MEDIACTRL'] = self.options.mediactrl
         if self.settings.os != 'Windows':
             cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
         if self.settings.os == 'Linux':
             # TODO : GTK3
             # cmake.definitions['wxBUILD_TOOLKIT'] = 'gtk3'
             cmake.definitions['wxUSE_CAIRO'] = self.options.cairo
+
+        # 3rd-party libraries
+        cmake.definitions['wxUSE_LIBPNG'] = option_value(self.options.png)
+        cmake.definitions['wxUSE_LIBJPEG'] = option_value(self.options.jpeg)
+        cmake.definitions['wxUSE_LIBTIFF'] = option_value(self.options.tiff)
+        cmake.definitions['wxUSE_ZLIB'] = option_value(self.options.zlib)
+        cmake.definitions['wxUSE_EXPAT'] = option_value(self.options.expat)
+
+        # wxWidgets features
+        cmake.definitions['wxUSE_UNICODE'] = self.options.unicode
+        cmake.definitions['wxUSE_SECRETSTORE'] = self.options.secretstore
+
+        # wxWidgets libraries
+        cmake.definitions['wxUSE_AUI'] = self.options.aui
+        cmake.definitions['wxUSE_OPENGL'] = self.options.opengl
+        cmake.definitions['wxUSE_HTML'] = self.options.html
+        cmake.definitions['wxUSE_MEDIACTRL'] = self.options.mediactrl
+        cmake.definitions['wxUSE_PROPGRID'] = self.options.propgrid
+        cmake.definitions['wxUSE_DEBUGREPORT'] = self.options.debugreport
+        cmake.definitions['wxUSE_RIBBON'] = self.options.ribbon
+        cmake.definitions['wxUSE_RICHTEXT'] = self.options.richtext
+        cmake.definitions['wxUSE_SOCKETS'] = self.options.sockets
+        cmake.definitions['wxUSE_STC'] = self.options.stc
+        cmake.definitions['wxUSE_WEBVIEW'] = self.options.webview
+        cmake.definitions['wxUSE_XML'] = self.options.xml
+        cmake.definitions['wxUSE_XRC'] = self.options.xrc
+
         cmake.configure(build_folder=self.build_subfolder)
         return cmake
 
@@ -153,10 +197,74 @@ class wxWidgetsConan(ConanFile):
         cmake = self.configure_cmake()
         cmake.install()
         # copy setup.h
-        self.copy(pattern='*setup.h', dst=os.path.join('include', 'wx'), src=os.path.join(self.build_subfolder, 'lib'), keep_path=False)
+        self.copy(pattern='*setup.h', dst=os.path.join('include', 'wx'), src=os.path.join(self.build_subfolder, 'lib'),
+                  keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        version_tokens = self.version.split('.')
+        version_major = version_tokens[0]
+        version_minor = version_tokens[1]
+        unicode = 'u' if self.options.unicode else ''
+        debug = 'd' if self.settings.build_type == 'Debug' else ''
+        if self.settings.os == 'Linux':
+            prefix = 'wx_'
+            toolkit = 'gtk2'
+            version = ''
+            suffix = '-%s.%s' % (version_major, version_minor)
+        elif self.settings.os == 'Macos':
+            prefix = 'wx_'
+            toolkit = 'osx_cocoa'
+            version = ''
+            suffix = '-%s.%s' % (version_major, version_minor)
+        elif self.settings.os == 'Windows':
+            prefix = 'wx'
+            toolkit = 'msw'
+            version = '%s%s' % (version_major, version_minor)
+            suffix = ''
+
+        def base_library_pattern(library):
+            return '{prefix}base{version}{unicode}{debug}_%s{suffix}' % library
+
+        def library_pattern(library):
+            return '{prefix}{toolkit}{version}{unicode}{debug}_%s{suffix}' % library
+
+        libs = ['{prefix}base{version}{unicode}{debug}{suffix}',
+                library_pattern('core'),
+                library_pattern('adv')]
+        if self.options.sockets:
+            libs.append(base_library_pattern('net'))
+        if self.options.xml:
+            libs.append(base_library_pattern('xml'))
+        if self.options.aui:
+            libs.append(library_pattern('aui'))
+        if self.options.opengl:
+            libs.append(library_pattern('gl'))
+        if self.options.html:
+            libs.append(library_pattern('html'))
+        if self.options.mediactrl:
+            libs.append(library_pattern('media'))
+        if self.options.propgrid:
+            libs.append(library_pattern('propgrid'))
+        if self.options.debugreport:
+            libs.append(library_pattern('qa'))
+        if self.options.ribbon:
+            libs.append(library_pattern('ribbon'))
+        if self.options.richtext:
+            libs.append(library_pattern('richtext'))
+        if self.options.stc:
+            libs.append(library_pattern('stc'))
+        if self.options.webview:
+            libs.append(library_pattern('webview'))
+        if self.options.xrc:
+            libs.append(library_pattern('xrc'))
+        for lib in libs:
+            self.cpp_info.libs.append(lib.format(prefix=prefix,
+                                                 toolkit=toolkit,
+                                                 version=version,
+                                                 unicode=unicode,
+                                                 debug=debug,
+                                                 suffix=suffix))
+
         self.cpp_info.defines.append('wxUSE_GUI=1')
         if self.settings.build_type == 'Debug':
             self.cpp_info.defines.append('__WXDEBUG__')
