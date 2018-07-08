38d0e6fbe6fc470ae2bc5116518f7e1fb98af0d3
SSE4 <tomskside@gmail.com>
2018-07-08 20:52:08 +0700

    - use ninja
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- build.py
+++ build.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
+import os
+from conans import tools
 from bincrafters import build_template_default
+
+def add_build_requires(builds):
+    return map(add_required_installers, builds)
+
+def add_required_installers(build):
+    installers = ['ninja_installer/1.8.2@bincrafters/stable']
+    build.build_requires.update({"*" : installers})
+    return build
 
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
+
+    builder.items = add_build_requires(builder.items)
 
     builder.run()
