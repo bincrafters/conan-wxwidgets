7635e377efada9f77fc351833f0652129327d346
nkshigeru <1712694+nkshigeru@users.noreply.github.com>
2019-03-27 14:44:06 +0900

    Fix unresolved reference error when using STC
    commit f0f47d31a86b7c66e60bc52390d8920fcc72c17b
    Author: nkshigeru <1712694+nkshigeru@users.noreply.github.com>
    Date:   Wed Mar 27 01:05:24 2019 +0900
    
        Change link order to fix build errors on linux
    
    commit 62c0013a6c260090d4ef32747cb3257708daea63
    Author: nkshigeru <1712694+nkshigeru@users.noreply.github.com>
    Date:   Tue Mar 26 21:34:21 2019 +0900
    
        Append wxscintilla.lib only for static library
    
    commit 3fee6eeb3ec3bbaa9f1fb096a15ad3959a529ed9
    Author: nkshigeru <1712694+nkshigeru@users.noreply.github.com>
    Date:   Tue Mar 26 12:58:42 2019 +0900
    
        Test wxStyledTextCtrl
    
    commit d3a302d349f6f1eecb96441d7fb80e2475719b48
    Author: nkshigeru <1712694+nkshigeru@users.noreply.github.com>
    Date:   Tue Mar 26 10:42:05 2019 +0900
    
        Add scintilla lib

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <wx/utils.h>
 #include <wx/init.h>
+#if wxUSE_STC
+#include <wx/stc/stc.h>
+#endif
 
 int main()
 {
@@ -16,6 +19,9 @@ int main()
     std::cout << vi.GetMajor() << ".";
     std::cout << vi.GetMinor() << ".";
     std::cout << vi.GetMicro() << std::endl;
+#if wxUSE_STC
+    wxStyledTextCtrl * stc = new wxStyledTextCtrl();
+#endif
     wxEntryCleanup();
     return EXIT_SUCCESS;
 }
