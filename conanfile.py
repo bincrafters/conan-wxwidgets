0b17d756ed5eea3c5464642c42a022b0ed9bca5b
bincrafters-user <bincrafters@gmail.com>
2020-09-30 12:09:24 +0000

    Update Conan conventions
    Automatically created by bincrafters-conventions 0.26.1

--- conanfile.py
+++ conanfile.py
@@ -135,7 +135,7 @@ class wxWidgetsConan(ConanFile):
                     installer.install(package)
 
     def build_requirements(self):
-        self.build_requires("ninja/1.10.0")
+        self.build_requires("ninja/1.10.1")
 
     def requirements(self):
         if self.options.png == 'libpng':
